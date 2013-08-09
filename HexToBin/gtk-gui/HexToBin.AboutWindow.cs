384c28d45fb251c1e4ed37ba95052e0b1c1eb035
vowstar <vowstar@gmail.com>
2013-08-09 22:00:25 +0800

    * Fix bug About Missing method System.Type::op_Inequality

--- HexToBin/gtk-gui/HexToBin.AboutWindow.cs
+++ HexToBin/gtk-gui/HexToBin.AboutWindow.cs
@@ -5,11 +5,8 @@ namespace HexToBin
 	public partial class AboutWindow
 	{
 		private global::Gtk.Table tableMain;
-
 		private global::Gtk.Image imageLogo;
-
 		private global::Gtk.Label labelTitle;
-
 		private global::Gtk.Button buttonOk;
 
 		protected virtual void Build ()
@@ -33,18 +30,18 @@ namespace HexToBin
 			this.imageLogo.Name = "imageLogo";
 			this.imageLogo.Pixbuf = global::Gdk.Pixbuf.LoadFromResource ("HexToBin.icon.main.hextobin_192x192.png");
 			this.tableMain.Add (this.imageLogo);
-			global::Gtk.Table.TableChild w2 = ((global::Gtk.Table.TableChild)(this.tableMain[this.imageLogo]));
+			global::Gtk.Table.TableChild w2 = ((global::Gtk.Table.TableChild)(this.tableMain [this.imageLogo]));
 			w2.XOptions = ((global::Gtk.AttachOptions)(4));
 			// Container child tableMain.Gtk.Table+TableChild
 			this.labelTitle = new global::Gtk.Label ();
 			this.labelTitle.Name = "labelTitle";
 			this.labelTitle.LabelProp = global::Mono.Unix.Catalog.GetString ("<span font_desc='32'>Hex转Bin工具</span>\n<span font_desc='16'>版本:{VERSION}</span>\n<span font_desc='16'>协议:GNU GPL V3</span>\n\n<span font_desc='16'>黄锐,兰州大学</span>\n<span font_desc='16'>vowstar@gmail.com</span>");
 			this.tableMain.Add (this.labelTitle);
-			global::Gtk.Table.TableChild w3 = ((global::Gtk.Table.TableChild)(this.tableMain[this.labelTitle]));
+			global::Gtk.Table.TableChild w3 = ((global::Gtk.Table.TableChild)(this.tableMain [this.labelTitle]));
 			w3.LeftAttach = ((uint)(1));
 			w3.RightAttach = ((uint)(2));
 			w1.Add (this.tableMain);
-			global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(w1[this.tableMain]));
+			global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(w1 [this.tableMain]));
 			w4.Position = 0;
 			w4.Expand = false;
 			w4.Fill = false;
@@ -63,7 +60,7 @@ namespace HexToBin
 			this.buttonOk.UseUnderline = true;
 			this.buttonOk.Label = "gtk-ok";
 			this.AddActionWidget (this.buttonOk, -5);
-			global::Gtk.ButtonBox.ButtonBoxChild w6 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w5[this.buttonOk]));
+			global::Gtk.ButtonBox.ButtonBoxChild w6 = ((global::Gtk.ButtonBox.ButtonBoxChild)(w5 [this.buttonOk]));
 			w6.Expand = false;
 			w6.Fill = false;
 			if ((this.Child != null)) {
